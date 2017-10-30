c65f8088b3ac349f3be488e07b418ce7b8c8f82c
Denallix Administrator <administrator@denallix.com>
2017-10-29 18:37:01 -0700

    running through examles of Queies, Models, Visualation

--- 1PSDSwR/1PSDSwR/Script.R
+++ 1PSDSwR/1PSDSwR/Script.R
@@ -6,7 +6,3 @@ x
 f <- function(x) { x + 1 }
 f(2)
 
-a <- array(
-        data = 1:8,
-        dim = c(2,2,2)
-           )
--- 1PSDSwR/data/m4/3 - Creating Descriptive Statistics.R
+++ 1PSDSwR/data/m4/3 - Creating Descriptive Statistics.R
@@ -1,13 +1,89 @@
 # Creating Descriptive Statistics
 
 # Set the working directory
-setwd("C:/Pluralsight")
+setwd("C:/1FabsRepo/StepIntoR/1PSDSwR/data/m4")
+
+
 
 # Read a CSV data file
 cars <- read.csv("Cars.csv")
 
 # Peek at the data
 head(cars)
+
+# Work with just a subset of items by doign a select
+temp = select(
+   .data = cars,
+   Transmission,
+   Cylinders,
+   Fuel.Economy)
+
+#just return whats in the filter now
+head(temp)
+
+#Apply a filter in the subset of data for just Automatic
+ temp <- filter(
+                .data = temp,
+               Transmission == "Automatic")
+
+#Compute a new column. This is an example of Transform
+temp <- mutate(
+        .data = temp,
+        Consumption = Fuel.Economy * 0.425
+               )
+
+#Inspect the results
+head(temp)
+
+#do a group by
+temp <- group_by(
+                 .data = cars,
+                 Cylinders)
+
+#check groupby values
+head(temp)
+
+
+#Aggregare based on groups
+temp <- summarize(
+        .data = temp,
+        Avg.Consumption = mean(Consumption))
+
+#check results
+head(temp)
+
+#Arrante the rows in decending order
+temp <- arrange(
+                .data = temp,
+                desc(Avg.Consumption))
+
+#check results
+head(temp)
+
+#convert data back to a data frame
+efficiency <- as.data.frame(temp)
+
+#check the results for new variable efficiency
+print(efficiency)
+
+#example of method chaining by Piping in C# that is || but in R it is %>%
+efficiency <- cars %>%
+    select(Fuel.Economy, Cylinders, Transmission) %>%
+    filter(Transmission == "Automatic") %>%
+    mutate(Consumption = Fuel.Economy * 0.425) %>%
+    group_by(Cylinders) %>%
+    summarise(Avg.Consumption = mean(Consumption)) %>%
+    as.data.frame()
+
+#Export or the Load in ETL now
+#save the results to a CSV file
+write.csv(
+          x = efficiency,
+          file = "Fuel Efficiency.csv",
+          row.names = FALSE)
+
+#check the results for new variable efficiency
+print(efficiency)
 
 # Create a frequency table
 table(cars$Transmission)
@@ -39,4 +115,48 @@ cor(
     y = cars$Fuel.Economy)
 
 # Summarize an entire table
-summary(cars)
+
+
+
+# do soem data visualizaiton now
+
+library(ggplot2)
+
+# Create a frequency bar chart
+ggplot(
+    data = cars,
+    aes(x = Transmission)) +
+    geom_bar() +
+    ggtitle("Count of Cars by Transmission Type") +
+    xlab("Transmission Type") +
+    ylab("Count of Cars")
+
+# Create a histogram
+ggplot(
+    data = cars,
+    aes(x = Fuel.Economy)) +
+    geom_histogram(
+        bins = 10) +
+        ggtitle("Distribution of Fuel Economy") +
+        xlab("Fuel Economy (mpg)") +
+        ylab("Count of Cars")
+
+# Create a density plot
+ggplot(
+    data = cars,
+    aes(x = Fuel.Economy)) +
+    geom_density() +
+    ggtitle("Distribution of Fuel Economy") +
+    xlab("Fuel Economy (mpg)") +
+    ylab("Density")
+
+# Create a scatterplot
+ggplot(
+    data = cars,
+    aes(
+        x = Cylinders,
+        y = Fuel.Economy)) +
+        geom_point() +
+        ggtitle("Fuel Economy by Cylinders") +
+        xlab("Number of Cylinders") +
+        ylab("Fuel Economy (mpg)")
